b6e49c1cc709e9f4b9fe88cd04f373da80f5f0f4
Gabanikeyur <gabanikeyur20@gmail.com>
2024-09-14 18:00:07 +0530

    Initial Setup

---
+++ BusinessLogic/IUserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+    
+namespace BusinessLogic
+{
+    public interface IUserService
+    {
+        void AddUser(User user);
+
+        void UpdateUser(User user);
+
+        void DeleteUser(int id);
+
+        User GetUserById(int id);
+
+        List<User> GetAllUsers();
+
+        List<City> GetCitiesByState(int stateId);
+
+        List<State> GetStates();
+    }
+}
---
+++ BusinessLogic/UserService.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class UserService : IUserService
+    {
+
+        UserRepository userRepository = new UserRepository();
+
+        public void AddUser(User user)
+        {
+            userRepository.InsertUser(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            userRepository.UpdateUser(user);
+        }
+
+        public void DeleteUser(int id)
+        {
+            userRepository.DeleteUser(id);
+        }
+
+        public User GetUserById(int id)
+        {
+            return userRepository.GetUserById(id);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return userRepository.GetAllUsers();
+        }
+
+        public List<City> GetCitiesByState(int stateId)
+        {
+            return userRepository.GetCitiesByState(stateId);
+        }
+
+        public List<State> GetStates()
+        {
+            return userRepository.GetStates();
+        }
+    }
+}
